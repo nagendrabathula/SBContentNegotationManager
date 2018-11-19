fe4b490c28321ebfcb83bcef28782f61f6f7f189
DELL <DELL@DESKTOP-B8MP7LK>
2018-11-19 17:26:02 +0530

    SB Content Negotation Manager

---
+++ boot-contentnegotiationmanager/src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.javainuse;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+}
+
+
+// Resource Url  : https://www.javainuse.com/spring/spring-boot-content-negotiation
---
+++ boot-contentnegotiationmanager/src/main/java/com/javainuse/config/WebConfig.java
@@ -0,0 +1,55 @@
+package com.javainuse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	
+	// Using Path Extension	
+	
+	/*@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	configurer.favorPathExtension(false).
+	    favorParameter(true).
+	    parameterName("mediaType").
+	    ignoreAcceptHeader(true).
+	    useJaf(false).
+	    defaultContentType(MediaType.APPLICATION_XML).
+	    mediaType("xml", MediaType.APPLICATION_XML).
+	    mediaType("json", MediaType.APPLICATION_JSON);
+	  }
+	}*/
+	
+	/*
+	 * Sample Urls
+	http://localhost:8080/employee.json
+    http://localhost:8080/employee.json
+*/	
+	
+	
+		// Using Path Parameters	
+	
+		@Override
+		public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorPathExtension(false).
+		    favorParameter(true).
+		    parameterName("mediaType").
+		    ignoreAcceptHeader(true).
+		    useJaf(false).
+		    defaultContentType(MediaType.APPLICATION_XML).
+		    mediaType("xml", MediaType.APPLICATION_XML).
+		    mediaType("json", MediaType.APPLICATION_JSON);
+		  }
+		}
+		
+		/*
+		 * Sample Urls
+		 * http://localhost:8080/employee?mediaType=json
+		   	
+		*/
